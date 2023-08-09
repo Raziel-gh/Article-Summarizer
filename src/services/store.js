9f38b8edf6180d5ab18b445f91a9b673387a9d1e
Giridhar Balasubramanian <84963158+Raziel-gh@users.noreply.github.com>
2023-08-09 15:07:16 +0530

    Add files via upload

---
+++ src/services/store.js
@@ -0,0 +1,12 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { articleApi } from "./article.js";
+import { useLazyGetSummaryQuery } from "./article.js";
+export const store = configureStore({
+    reducer:{
+        [articleApi.reducerPath]:articleApi.reducer
+    },
+    middleware: (getDefaultMiddleware)=>
+        getDefaultMiddleware().concat(articleApi.middleware),
+    
+
+})
